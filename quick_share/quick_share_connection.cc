ff20a1b69ba25bc480b3090109305b112a10db69
Or Yair <or.yair@safebreach.com>
2024-06-19 18:24:37 +0300

    added usage of the new logger in quick share connection and quick share medium

--- quick_share/quick_share_connection.cc
+++ quick_share/quick_share_connection.cc
@@ -1,4 +1,5 @@
-#include "quick_share/include/quick_share_connection.hh"
+#include "include/quick_share_connection.hh"
+#include "logger/include/logger.hh"
 
 #include <string>
 #include <memory>
@@ -7,138 +8,146 @@
 
 static const size_t VERIFICATION_STRING_LENGTH = 32;
 
-void QuickShareConnection::connect() {
+void QuickShareConnection::connect()
+{
     m_medium->connect();
 }
 
-void QuickShareConnection::disconnect() {
+void QuickShareConnection::disconnect()
+{
     m_medium->disconnect();
 }
 
-void QuickShareConnection::set_medium(IMedium * medium) {
+void QuickShareConnection::set_medium(IMedium *medium)
+{
     m_medium = medium;
 }
 
-void QuickShareConnection::send_packet(const char * packet, size_t packet_size) {
+void QuickShareConnection::send_packet(const char *packet, size_t packet_size)
+{
     uint32_t packet_size_big_endian = htonl(packet_size);
-    m_medium->send((char*)&packet_size_big_endian, sizeof(uint32_t));
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Sending packet size packet through medium");
+    m_medium->send((char *)&packet_size_big_endian, sizeof(uint32_t));
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Sending packet itself through medium");
     m_medium->send(packet, packet_size);
 }
 
-std::vector<char> QuickShareConnection::recv_packet() {
+std::vector<char> QuickShareConnection::recv_packet()
+{
     std::vector<char> packet_size_bytes_big_endian = m_medium->recv(sizeof(uint32_t));
-    size_t packet_size = ntohl(*(uint32_t*)packet_size_bytes_big_endian.data());
+    size_t packet_size = ntohl(*(uint32_t *)packet_size_bytes_big_endian.data());
 
     return m_medium->recv(packet_size);
 }
 
-void QuickShareConnection::send_offline_frame(OfflineFrame & offline_frame, bool should_encrypt) {
+void QuickShareConnection::send_offline_frame(OfflineFrame &offline_frame, bool should_encrypt)
+{
     std::string offline_frame_bytes(offline_frame.SerializeAsString());
-    printf("Sending offline frame:\n");
-    printf(offline_frame.DebugString().c_str());
+    logger_log(LoggerLogLevel::LEVEL_DEBUG, "Sending offline frame:");
+    logger_log(LoggerLogLevel::LEVEL_DEBUG, offline_frame.DebugString().c_str());
 
-    if (should_encrypt) {
+    if (should_encrypt)
+    {
         std::unique_ptr<std::string> encoded_packet = m_connection_context->EncodeMessageToPeer(offline_frame_bytes);
         send_packet(encoded_packet->c_str(), encoded_packet->length());
-    } else {
+    }
+    else
+    {
         send_packet(offline_frame_bytes.c_str(), offline_frame_bytes.length());
     }
-
 }
 
-std::unique_ptr<OfflineFrame> QuickShareConnection::recv_offline_frame(bool should_decrypt) {
+std::unique_ptr<OfflineFrame> QuickShareConnection::recv_offline_frame(bool should_decrypt)
+{
     std::vector<char> packet_bytes = recv_packet();
     std::string packet_bytes_string(packet_bytes.begin(), packet_bytes.end());
 
     auto offline_frame = std::make_unique<OfflineFrame>();
-    
-    if (should_decrypt) {
+
+    if (should_decrypt)
+    {
         std::unique_ptr<std::string> decrypted_offline_frame_bytes = m_connection_context->DecodeMessageFromPeer(packet_bytes_string);
-        if (!decrypted_offline_frame_bytes) {
+        if (!decrypted_offline_frame_bytes)
+        {
             return nullptr;
         }
         offline_frame->ParseFromString(*decrypted_offline_frame_bytes);
-    } else {
+    }
+    else
+    {
         offline_frame->ParseFromString(packet_bytes_string);
     }
 
-    
-    printf("Received offline frame:\n");
-    printf(offline_frame->DebugString().c_str());
+    logger_log(LoggerLogLevel::LEVEL_DEBUG, "Received offline frame:");
+    logger_log(LoggerLogLevel::LEVEL_DEBUG, offline_frame->DebugString().c_str());
 
     return offline_frame;
 }
 
-void QuickShareConnection::do_handshake() {
+void QuickShareConnection::do_handshake()
+{
     string packet_from_responder_string;
     std::vector<char> packet_from_responder;
     std::unique_ptr<string> auth_string = NULL;
     std::unique_ptr<string> packet_to_responder = nullptr;
     UKey2Handshake::ParseResult result;
 
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Initiating Ukey2 Handshake");
+
     m_ukey2_handshake = UKey2Handshake::ForInitiator(UKey2Handshake::HandshakeCipher::P256_SHA512);
-    if (nullptr == m_ukey2_handshake) {
+    if (nullptr == m_ukey2_handshake)
+    {
         throw Ukey2Exception("ERROR: Failed creating the Ukey2Handshake object");
     }
-    printf("handshake stage 1\n");
 
     packet_to_responder = m_ukey2_handshake->GetNextHandshakeMessage();
-    if (nullptr == packet_to_responder) {
+    if (nullptr == packet_to_responder)
+    {
         throw Ukey2Exception("ERROR: Failed getting first handshake message to send");
     }
 
-    printf("handshake stage 2\n");
-
+    logger_log(LoggerLogLevel::LEVEL_DEBUG, "Sending Ukey2 Client Init");
     send_packet(packet_to_responder->c_str(), packet_to_responder->length());
 
-    printf("handshake stage 3\n");
-
+    logger_log(LoggerLogLevel::LEVEL_DEBUG, "Receiving Ukey2 Server Init");
     packet_from_responder = recv_packet();
-
-    printf("handshake stage 4\n");
-
     packet_from_responder_string = std::string(packet_from_responder.begin(), packet_from_responder.end());
 
-    printf("handshake stage 5\n");
-
     result = m_ukey2_handshake->ParseHandshakeMessage(packet_from_responder_string);
-    if (!result.success) {
-        if (result.alert_to_send) {
-                send_packet(result.alert_to_send->c_str(), result.alert_to_send->length());
+    if (!result.success)
+    {
+        if (result.alert_to_send)
+        {
+            send_packet(result.alert_to_send->c_str(), result.alert_to_send->length());
         }
         throw Ukey2Exception("ERROR: Failed parsing first handshake message received from the other side");
     }
 
-    printf("handshake stage 6\n");
-
     packet_to_responder = m_ukey2_handshake->GetNextHandshakeMessage();
-    if (nullptr == packet_to_responder) {
+    if (nullptr == packet_to_responder)
+    {
         throw Ukey2Exception("Failed getting second handshake message to send");
     }
 
-    printf("handshake stage 7\n");
-    
+    logger_log(LoggerLogLevel::LEVEL_DEBUG, "Sending Ukey2 Client Finish");
     send_packet(packet_to_responder->c_str(), packet_to_responder->length());
 
-    printf("handshake stage 8\n");
-
     auth_string = m_ukey2_handshake->GetVerificationString(VERIFICATION_STRING_LENGTH);
-    if (nullptr == auth_string) {
+    if (nullptr == auth_string)
+    {
         throw Ukey2Exception("Failed getting the verification string");
     }
 
-    printf("handshake stage 9\n");
-
-    if (false == m_ukey2_handshake->VerifyHandshake()) {
+    if (false == m_ukey2_handshake->VerifyHandshake())
+    {
         throw Ukey2Exception("Failed verifying the handshake");
     }
 
-    printf("handshake stage 10\n");
-    
     m_connection_context = m_ukey2_handshake->ToConnectionContext();
-    if (nullptr == m_connection_context) {
+    if (nullptr == m_connection_context)
+    {
         throw Ukey2Exception("Failed getting the connection context object");
     }
 
-    printf("handshake stage 11\n");
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Ukey2 Handshake finished successfully");
 }
--- quick_share/quick_share_medium.cc
+++ quick_share/quick_share_medium.cc
@@ -1,4 +1,5 @@
 #include "quick_share/include/quick_share_medium.hh"
+#include "logger/include/logger.hh"
 
 #include <cstdlib>
 #include <vector>
@@ -9,126 +10,151 @@
 static void str2ba(const char *straddr, BTH_ADDR *btaddr);
 static void initialize_wsa();
 
-static void initialize_wsa() {
+static void initialize_wsa()
+{
     WSADATA wsa;
-    if (0 != WSAStartup(MAKEWORD(2, 2), &wsa)) {
+    if (0 != WSAStartup(MAKEWORD(2, 2), &wsa))
+    {
         throw SocketException("Call to WSAStartup failed");
     }
 }
 
-static void str2ba(const char *straddr, BTH_ADDR *btaddr) {
+static void str2ba(const char *straddr, BTH_ADDR *btaddr)
+{
     int i;
     unsigned int aaddr[6];
     BTH_ADDR tmpaddr = 0;
 
     if (sscanf(straddr, "%02x:%02x:%02x:%02x:%02x:%02x",
-                    &aaddr[0], &aaddr[1], &aaddr[2],
-                    &aaddr[3], &aaddr[4], &aaddr[5]) != 6) {
+               &aaddr[0], &aaddr[1], &aaddr[2],
+               &aaddr[3], &aaddr[4], &aaddr[5]) != 6)
+    {
         throw InvalidParameterException("MAC address is not in the right format");
     }
     *btaddr = 0;
-    for (i = 0; i < 6; i++) {
-        tmpaddr = (BTH_ADDR) (aaddr[i] & 0xff);
+    for (i = 0; i < 6; i++)
+    {
+        tmpaddr = (BTH_ADDR)(aaddr[i] & 0xff);
         *btaddr = ((*btaddr) << 8) + tmpaddr;
     }
 }
 
-BaseSocketMedium::BaseSocketMedium() {
+BaseClientSocketMedium::BaseClientSocketMedium()
+{
     initialize_wsa();
 }
 
-void BaseSocketMedium::connect() {
+void BaseClientSocketMedium::connect()
+{
     SOCKET specific_socket = INVALID_SOCKET;
     unsigned int timeout = 10000;
     int ret_val = -1;
-    SOCKADDR * socket_address = get_socket_address();
+    SOCKADDR *socket_address = get_socket_address();
     size_t socket_address_size = get_socket_address_size();
 
-    printf("connect: stage1\n");
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Setting up medium socket");
     specific_socket = create_socket();
-    if (INVALID_SOCKET == specific_socket) {
+    if (INVALID_SOCKET == specific_socket)
+    {
         throw SocketException("ERROR: Failed initializing Wifi Lan socket");
     }
-    printf("connect: stage2\n");
 
-    ret_val = setsockopt(specific_socket, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout));
-    if (SOCKET_ERROR == ret_val) {
+    logger_log(LoggerLogLevel::LEVEL_DEBUG, "Setting socket timeout to %u milliseconds", timeout);
+    ret_val = setsockopt(specific_socket, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
+    if (SOCKET_ERROR == ret_val)
+    {
         closesocket(specific_socket);
         throw SocketException("ERROR: Failed setting timeout for Wifi Lan socket");
     }
-    printf("connect: stage3\n");
 
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Connecting to medium socket");
     ret_val = ::connect(specific_socket, socket_address, socket_address_size);
-    if (SOCKET_ERROR == ret_val) {
+    if (SOCKET_ERROR == ret_val)
+    {
         closesocket(specific_socket);
         throw SocketException("ERROR: Failed connecting to target");
-    } 
-    printf("connect: stage4\n");
+    }
 
     m_socket = specific_socket;
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Successfully connected to medium socket");
 }
 
-void BaseSocketMedium::disconnect() {
-    if (INVALID_SOCKET != m_socket) {
+void BaseClientSocketMedium::disconnect()
+{
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Disconnecting from medium socket");
+    if (INVALID_SOCKET != m_socket)
+    {
         closesocket(m_socket);
     }
 }
 
-void BaseSocketMedium::send(const char * buffer, size_t buffer_size) {
-    if (SOCKET_ERROR == ::send(m_socket, (char *)buffer, buffer_size, 0)) {
-        throw SocketException("ERROR: Failed sending buffer over socket");
+void BaseClientSocketMedium::send(const char *buffer, size_t buffer_size)
+{
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Sending packet through socket");
+    if (SOCKET_ERROR == ::send(m_socket, (char *)buffer, buffer_size, 0))
+    {
+        throw SocketException("Failed sending buffer over socket");
     }
 }
 
-std::vector<char> BaseSocketMedium::recv(size_t num_bytes_to_read) {
+std::vector<char> BaseClientSocketMedium::recv(size_t num_bytes_to_read)
+{
     std::vector<char> received_data(num_bytes_to_read);
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Receiving packet from socket");
     int result = ::recv(m_socket, (char *)received_data.data(), num_bytes_to_read, 0);
 
-    if (SOCKET_ERROR == result) {
+    if (SOCKET_ERROR == result)
+    {
         int wsa_error_code = WSAGetLastError();
-        if (WSAETIMEDOUT == wsa_error_code) {
-            printf("Recv timeout!\n");
+        if (WSAETIMEDOUT == wsa_error_code)
+        {
             throw TimeoutException("Receiving from socket timed out");
-        } else {
-            printf("Another recv err!\n");
-            throw SocketException("ERROR: Failed receiving buffer from socket");
+        }
+        else
+        {
+            throw SocketException("Failed receiving buffer from socket");
         }
     }
-    else if (0 == result){
-        printf("Socket gracefully closed\n");
-        throw SocketException("ERROR: Socket is closed");
+    else if (0 == result)
+    {
+        throw SocketException("Socket was gracefully closed");
     }
 
     return received_data;
 }
 
-void WifiLanMedium::set_target(const char * target_ip, unsigned int target_port) {
+void WifiLanMedium::set_target(const char *target_ip, unsigned int target_port)
+{
     m_sock_addr.sin_family = AF_INET;
     m_sock_addr.sin_port = htons(target_port);
     m_sock_addr.sin_addr.S_un.S_addr = inet_addr(target_ip);
 }
 
-SOCKET WifiLanMedium::create_socket() {
+SOCKET WifiLanMedium::create_socket()
+{
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Creating WIFI_LAN socket");
     SOCKET wifi_lan_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
 
-    if (INVALID_SOCKET == wifi_lan_socket) {
-        printf("happens\n");
-        throw SocketException("ERROR: Couldn't create a TCP socket");
+    if (INVALID_SOCKET == wifi_lan_socket)
+    {
+        throw SocketException("Couldn't create a TCP socket");
     }
-    printf("create_socket: stage3\n");
 
     return wifi_lan_socket;
 }
 
-SOCKADDR * WifiLanMedium::get_socket_address() {
-    return (SOCKADDR*)&m_sock_addr;
+SOCKADDR *WifiLanMedium::get_socket_address()
+{
+    return (SOCKADDR *)&m_sock_addr;
 }
 
-size_t WifiLanMedium::get_socket_address_size() {
+size_t WifiLanMedium::get_socket_address_size()
+{
     return sizeof(m_sock_addr);
 }
 
-void BluetoothMedium::set_target(const char * target_mac_address, GUID target_service_class_id) {
+void BluetoothMedium::set_target(const char *target_mac_address, GUID target_service_class_id)
+{
     BTH_ADDR btAddr;
 
     m_bluetooth_server_addr.addressFamily = AF_BTH;
@@ -139,27 +165,33 @@ void BluetoothMedium::set_target(const char * target_mac_address, GUID target_se
 
     // Manually set the service class UUID
     // m_bluetooth_server_addr.serviceClassId =  {0xA82EFA21, 0xAE5C, 0x3DDE, {0x9B, 0xBC, 0xF1, 0x6D, 0xA7, 0xB1, 0x6C, 0x5A}};
-    m_bluetooth_server_addr.serviceClassId =  target_service_class_id;
+    m_bluetooth_server_addr.serviceClassId = target_service_class_id;
 }
 
-SOCKET BluetoothMedium::create_socket() {
+SOCKET BluetoothMedium::create_socket()
+{
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Creating BLUETOOTH socket");
     SOCKET bluetooth_socket = ::socket(AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
-    if (INVALID_SOCKET == bluetooth_socket) {
+    if (INVALID_SOCKET == bluetooth_socket)
+    {
         throw SocketException("ERROR: Couldn't create a bluetooth socket");
     }
 
     return bluetooth_socket;
 }
 
-SOCKADDR * BluetoothMedium::get_socket_address() {
+SOCKADDR *BluetoothMedium::get_socket_address()
+{
     return (SOCKADDR *)&m_bluetooth_server_addr;
 }
 
-size_t BluetoothMedium::get_socket_address_size() {
+size_t BluetoothMedium::get_socket_address_size()
+{
     return sizeof(m_bluetooth_server_addr);
 }
 
-unsigned int WifiHotspotMedium::bind(const char * hotspot_listen_ip, unsigned int listen_port) {
+unsigned int WifiHotspotMedium::bind(const char *hotspot_listen_ip, unsigned int listen_port)
+{
     SOCKET server_socket = INVALID_SOCKET;
     int ret_val = -1;
 
@@ -168,20 +200,24 @@ unsigned int WifiHotspotMedium::bind(const char * hotspot_listen_ip, unsigned in
     m_server_sock_addr.sin_addr.S_un.S_addr = inet_addr(hotspot_listen_ip);
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (INVALID_SOCKET == server_socket) {
+    if (INVALID_SOCKET == server_socket)
+    {
         throw SocketException("Failed creating a socket for WifiHotspotMedium");
     }
-    
-    ret_val = ::bind(server_socket, (SOCKADDR*)&m_server_sock_addr, sizeof(m_server_sock_addr));
-    if (SOCKET_ERROR == ret_val) {
+
+    ret_val = ::bind(server_socket, (SOCKADDR *)&m_server_sock_addr, sizeof(m_server_sock_addr));
+    if (SOCKET_ERROR == ret_val)
+    {
         closesocket(server_socket);
         throw SocketException("Failed binding WifiHotspotMedium to the listening address");
     }
 
-    if (0 == listen_port) {
+    if (0 == listen_port)
+    {
         int sock_addr_size = sizeof(m_server_sock_addr);
-        ret_val = getsockname(server_socket, (SOCKADDR*)&m_server_sock_addr, &sock_addr_size);
-        if (SOCKET_ERROR == ret_val) {
+        ret_val = getsockname(server_socket, (SOCKADDR *)&m_server_sock_addr, &sock_addr_size);
+        if (SOCKET_ERROR == ret_val)
+        {
             closesocket(server_socket);
             throw SocketException("Failed retrieving the socket address after a random available port was asked by the caller");
         }
@@ -191,40 +227,50 @@ unsigned int WifiHotspotMedium::bind(const char * hotspot_listen_ip, unsigned in
     return ntohs(m_server_sock_addr.sin_port);
 }
 
-void WifiHotspotMedium::connect() {
+void WifiHotspotMedium::connect()
+{
     int ret_val = -1;
     SOCKET client_socket;
     fd_set read_set;
     FD_ZERO(&read_set);
     FD_SET(m_server_socket, &read_set);
 
-
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Listening for WIFI_HOTSPOT connections");
     ret_val = listen(m_server_socket, 1);
-    if (SOCKET_ERROR == ret_val) {
+    if (SOCKET_ERROR == ret_val)
+    {
         throw SocketException("WifiHotspotMedium failed listening");
     }
 
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Selecting WIFI_HOTSPOT connections");
     ret_val = select(0, &read_set, NULL, NULL, &m_accept_timeout);
-    if (0 == ret_val) {
+    if (0 == ret_val)
+    {
         throw TimeoutException("Timed out waiting for the client to connect");
-    } else if (SOCKET_ERROR == ret_val) {
+    }
+    else if (SOCKET_ERROR == ret_val)
+    {
         throw SocketException("Failed waiting for the client to connect");
     }
 
     int sock_addr_size = sizeof(m_client_sock_addr);
-    client_socket = accept(m_server_socket, (SOCKADDR*)&m_client_sock_addr, &sock_addr_size);
-    if (INVALID_SOCKET == client_socket) {
+    client_socket = accept(m_server_socket, (SOCKADDR *)&m_client_sock_addr, &sock_addr_size);
+    if (INVALID_SOCKET == client_socket)
+    {
         throw SocketException("Failed accepting connection from client");
     }
 
     m_socket = client_socket;
 }
 
-void WifiHotspotMedium::disconnect() {
+void WifiHotspotMedium::disconnect()
+{
+    logger_log(LoggerLogLevel::LEVEL_INFO, "Disconnecting WIFI_HOTSPOT socket");
     closesocket(m_server_socket);
-    BaseSocketMedium::disconnect();
+    BaseClientSocketMedium::disconnect();
 }
 
-SOCKADDR_IN & WifiHotspotMedium::get_connected_client_sock_addr() {
+SOCKADDR_IN &WifiHotspotMedium::get_connected_client_sock_addr()
+{
     return m_client_sock_addr;
 }
