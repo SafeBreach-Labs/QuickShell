9a374947ce125dd55ed7de1e1477be39fce74d00
Or Yair <or.yair@safebreach.com>
2024-06-19 15:04:29 +0300

    added generic utils for the tools

---
+++ tools/include/utils.hh
@@ -0,0 +1,16 @@
+#ifndef TOOLS_INCLUDE_UTILS
+#define TOOLS_INCLUDE_UTILS
+
+#include <memory>
+#include <vector>
+#include <string>
+
+size_t get_file_size(const char * file_path);
+
+std::unique_ptr<std::vector<char>> read_file_content(const char * file_path);
+
+std::vector<char> base64_decode(const std::string &encoded_string);
+
+bool is_base64(char c);
+
+#endif /* TOOLS_INCLUDE_UTILS */
